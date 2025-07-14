24bebb0152d0580b6cb91779deca607faf64b139
Fabian Facal Biemmi <fabianfacalbiemmi@MacBook-Air-von-Fabian.local>
2025-07-14 10:48:54 +0200

    update() bug behoben

--- script.js
+++ script.js
@@ -117,17 +117,6 @@ function update(location) {
     button3.onclick = location["button functions"][2];
 
     text.innerHTML = location.text;
-
-    //Go Store
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-
-    text.innerText = "You enter the store.";
 }
 
 function goTown() {
